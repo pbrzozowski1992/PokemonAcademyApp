f20ecc73d502e4b7998993b2cc3b189f2d03dcb7
Piotr Brzozowski <piotr.brzozowski.dev@gmail.com>
2020-10-18 13:10:10 +0200

    feature(paa): bind pokemon list with pokemon api

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -2,17 +2,48 @@ import React from 'react';
 
 class PokemonList extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { pokemonList: [] };
+    }
+
     componentDidMount() {
         fetch('https://pokemon-academy-api-pbr.herokuapp.com/pokemons/list')
-        .then(response => {
-            console.log(response);
+            .then(response => response.json())
+            .then(jsonResponse => {
+                const results = jsonResponse.results;
+                this.setState({ pokemonList: results })
+            })
+    }
+
+    renderList = (pokemonList) => {
+        return pokemonList.map((pokemon, index) => {
+            const { imageUrl, name, level } = pokemon;
+            return <tr key={index}> 
+                <td><img src={imageUrl}/></td>
+                <td><p>{name}</p></td>
+                <td><p>{level}</p></td>
+            </tr>
         })
     }
 
+    renderHeader = () => {
+        return <tr>
+            <td><p>Image</p></td>
+            <td><p>Name</p></td>
+            <td><p>Level</p></td>
+        </tr>
+    }
 
     render() {
         return(
-            <h1>Pokemon List!</h1>
+            <div>
+                <h1>Pokemon List:</h1>
+                <table>
+                    {this.renderHeader()}
+                    {this.renderList(this.state.pokemonList)}
+                </table>
+            </div>
         )
     }
 }
